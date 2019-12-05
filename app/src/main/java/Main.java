b15d49c169b88a2901a6693bec456bafcec6e4dc
Arnout Engelen <arnout@bzzt.net>
2019-12-05 10:37:14 +0100

    test getting directly with 2.6-SNAPSHOT

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,12 +1,15 @@
 package app;
 
+import akka.remote.AddressUidExtension;
 import akka.actor.ActorSystem;
 import lib.Lib;
 
 class Main {
 	public static void main(String[] argv) {
 		ActorSystem system = ActorSystem.create();
-		Long uid = new Lib().main(system);
-		System.out.println(uid.toString());
+		//Long uid = new Lib().main(system);
+		AddressUidExtension e = AddressUidExtension.get(system);
+		//Long uid = e.uniqueAddress();
+		//System.out.println(uid.toString());
 	}
 }
